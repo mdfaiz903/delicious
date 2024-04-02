d8f732197b15483cdec0a25454a347f458e878d3
mdfaiz903 <mdfaiz.nub@gmail.com>
2024-04-03 01:29:05 +0600

    Django template inheritance done and all pages

--- delicious/urls.py
+++ delicious/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from . views import homeview
+from . views import homeview,aboutView,blogView,receipeView,contactView
 urlpatterns = [
-    path('',homeview,name='home')
+    path('',homeview,name='home'),
+    path('aboutView/',aboutView,name='aboutView'),
+    path('blogView/',blogView,name='blogView'),
+    path('receipeView/',receipeView,name='receipeView'),
+    path('contactView/',contactView,name='contactView'),
 ]
--- delicious/views.py
+++ delicious/views.py
@@ -5,3 +5,14 @@ from django.shortcuts import render
 
 def homeview(request):
     return render(request,'delicious/index.html')
+
+
+def aboutView(request):
+    return render(request,'delicious/about.html')
+def blogView(request):
+    return render(request,'delicious/blog-post.html')
+def receipeView(request):
+    return render(request,'delicious/receipe-post.html')
+def contactView(request):
+    return render(request,'delicious/contact.html')
+
