1fc89d12e7731bc1db045f8385930be64acdf290
mdfaiz903 <mdfaiz.nub@gmail.com>
2024-04-16 22:17:13 +0600

    page paginator added

--- delicious/views.py
+++ delicious/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from . models import sliderModel,BlogPost,BlogCategory
 from . forms import ContactForm
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
@@ -26,12 +26,20 @@ def aboutView(request):
 
     return render(request,'delicious/about.html')
 def blogView(request):
-    queryset = BlogPost.objects.all()
+    queryset = BlogPost.objects.all().order_by('-id')
     blog_item = BlogCategory.objects.all()
-
+    paginator = Paginator(queryset,3)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
     context = {
         'items':queryset,
-        'blog_items':blog_item
+        'blog_items':blog_item,
+        'page_obj':page_obj,
     }
     return render(request,'delicious/blog-post.html',context)
 def blog_details(request,id):
